5c24d4ac2714c0cc0d89715e0cfedf9c9a020484
ishtiaqa1 <ishtiaqa2003@gmail.com>
2025-10-08 11:30:24 -0400

    Finsihed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 //write your code here
+
+const findMax = (nums) => {
+    let max = nums[0];
+    for (let i = 1; i < nums.length; i++) {
+        max = Math.max(max, nums[i]);
+    }
+    return max;
+}
+
+console.log(findMax([3, 7, 2, 9, 5]));
+console.log(findMax([-1, -5, -3]));
